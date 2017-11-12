ff30ecb89f76a864c7e13fa71e3d90790db8067b
Felipe A Soares <felipe.soares@paxtecnologia.com.br>
2017-11-12 12:41:52 -0200

    api de teste retornando json

--- BibliotecaWAR/src/rest/Initializer.java
+++ BibliotecaWAR/src/rest/Initializer.java
@@ -3,5 +3,5 @@ package rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("/api")
 public class Initializer extends Application {}
--- BibliotecaWAR/src/rest/Teste.java
+++ BibliotecaWAR/src/rest/Teste.java
@@ -2,14 +2,17 @@ package rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
-@Path("/")
+@Path("/teste")
 public class Teste {
 	
-	@Path("/teste")
+	@Path("/")
 	@GET
+	@Produces("application/json")
 	public String teste() {
-		return "teste";
+		String retorno = "{\"ok\": \"funciona\"}";
+		return retorno;
 	}
 	
 }
